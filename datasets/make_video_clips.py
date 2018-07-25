5d63c5ac61c13a67e5863adc3cd2108f6cafd5bc
Ya-Liang Chang (Allen) <b03901014@ntu.edu.tw>
2018-07-25 16:50:18 +0800

    Add clips preview and saving

--- datasets/make_video_clips.py
+++ datasets/make_video_clips.py
@@ -5,6 +5,7 @@ from math import ceil
 
 import skvideo.io as skvio
 import cv2
+from moviepy.editor import VideoFileClip
 
 
 LOGGING_LEVEL = logging.INFO
@@ -62,7 +63,7 @@ def get_frames_from_video(video_filename):
             "Last frame is skipped due to a skvideo bug",
             exc_info=True
         )
-    return frames
+    return frames, int(frame_rate)
 
 
 def main(args):
@@ -70,18 +71,22 @@ def main(args):
     if not os.path.exists(args.video_path):
         raise IOError(f"Video {args.video_path} not exists")
 
-    frames = get_frames_from_video(args.video_path)
+    frames, frame_rate = get_frames_from_video(args.video_path)
+    video = VideoFileClip(args.video_path)
+    logger.info("Done")
 
     frame_idx = 0
-    interval = 30
+    interval = 15
     start_idx = 0
     end_idx = 0
+    output_count = 1
 
+    # video.show(frame_idx)
     running = True
     while running:
         frame = frames[frame_idx]
         # Display the resulting frame
-        cv2.imshow('frame', frame)
+        cv2.imshow('frame', cv2.cvtColor(frame, cv2.COLOR_RGB2BGR))
 
         ################
         # Key controls #
@@ -90,23 +95,63 @@ def main(args):
         k = cv2.waitKey(25)
 
         # Move backward
-        if k == ord('a'):
+        if k == ord('j'):
             frame_idx = (
                 frame_idx - interval
                 if frame_idx >= interval
                 else frame_idx
             )
+            logger.info(frame_idx)
+
+        if k == ord('h'):
+            frame_idx = (
+                frame_idx - interval * 2
+                if frame_idx >= interval * 2
+                else frame_idx
+            )
+            logger.info(frame_idx)
 
         # Move forward
-        elif k == ord('s'):
+        elif k == ord('k'):
             frame_idx = (
                 frame_idx + interval
                 if frame_idx < len(frames) - interval
                 else frame_idx
             )
+            logger.info(frame_idx)
+
+        elif k == ord('l'):
+            frame_idx = (
+                frame_idx + interval * 2
+                if frame_idx < len(frames) - interval * 2
+                else frame_idx
+            )
+            logger.info(frame_idx)
+
+        elif k == ord('d'):
+            start_idx = frame_idx
+            logger.info(f"Chang start: {start_idx}")
 
         elif k == ord('f'):
-            print(f"{start_idx},{end_idx}")
+            end_idx = frame_idx
+            logger.info(f"Change end: {end_idx}")
+
+        elif k == ord('s'):
+            if start_idx >= end_idx:
+                continue
+            video_clip = video.subclip(
+                start_idx / frame_rate,
+                end_idx / frame_rate
+            )
+            video_clip.preview()
+            video_dir = "".join(args.video_path.split('.mp4')[:-1])
+            if not os.path.exists(video_dir):
+                os.makedirs(video_dir)
+            output_filename = f"{video_dir}/{output_count:03}.mp4"
+            while os.path.exists(output_filename):
+                output_count += 1
+                output_filename = f"{video_dir}/{output_count:03}.mp4"
+            video_clip.write_videofile(output_filename)
 
         # Exit
         elif k == ord('q'):
